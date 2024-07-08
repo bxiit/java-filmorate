26d007f378e838de148c73ddf165b55cfc710062
bxit <arturivanov50808@gmail.com>
2024-07-09 04:49:34 +0500

    Удалить: аннотацию @SneakyThrows в классе запуска

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmorateApplication {
-    @SneakyThrows
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
     }
