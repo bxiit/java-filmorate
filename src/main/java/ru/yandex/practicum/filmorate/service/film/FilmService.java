26fd631f7b99c68dc58700bd5ef77e09b649b06e
bxit <arturivanov50808@gmail.com>
2024-07-19 15:04:03 +0500

    Добавить: Qualifier в конфиг lombok copyable annotations

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dto.director.DirectorDto;
 import ru.yandex.practicum.filmorate.dto.film.FilmDto;
-import ru.yandex.practicum.filmorate.dto.genre.GenreDto;
-import ru.yandex.practicum.filmorate.dto.mpa.MpaDto;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.mappers.DirectorMapper;
@@ -19,13 +17,13 @@ import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.director.DirectorService;
 import ru.yandex.practicum.filmorate.service.genre.GenreService;
 import ru.yandex.practicum.filmorate.service.mpa.MpaService;
-import ru.yandex.practicum.filmorate.storage.director.DirectorStorage;
+import ru.yandex.practicum.filmorate.storage.director.DirectorDBStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.genre.GenreDBStorage;
+import ru.yandex.practicum.filmorate.storage.mpa.MpaDBStorage;
 import ru.yandex.practicum.filmorate.util.SortBy;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -39,9 +37,15 @@ public class FilmService {
 
     private final MpaService mpaService;
 
+    private final MpaDBStorage mpaStorage;g
+
     private final GenreService genreService;
 
+    private final GenreDBStorage genreStorage;
+
     private final DirectorService directorService;
+
+    private final DirectorDBStorage directorStorage;
 
     public FilmDto addFilm(FilmDto request) {
         convertDuration(request);
@@ -52,23 +56,6 @@ public class FilmService {
         film = filmStorage.addFilm(film);
         addDirectorForFilm(film);
         return FilmMapper.MAPPER.mapToFilmDto(film);
-    }
-
-    private void convertDuration(FilmDto request) {
-        request.setDuration(request.getDuration().multipliedBy(60));
-    }
-
-    private void addDirectorForFilm(Film film) {
-        if (film.getDirectors() == null) {
-            return;
-        }
-        film.getDirectors().stream()
-                .map(DirectorDto::getId)
-                .forEach(directorId -> {
-                    DirectorDto directorDto = directorService.findDirectorById(directorId);
-                    Director director = DirectorMapper.MAPPER.mapToModel(directorDto);
-                    directorService.addDirectorForFilm(film.getId(), director);
-                });
     }
 
     public FilmDto findFilmById(long filmId) {
@@ -113,18 +100,8 @@ public class FilmService {
         return updatedFilmDto;
     }
 
-    // удаляет все связки режиссера с фильмом и добавляет заново
-    private void updateDirectorOfFilm(FilmDto filmDto) {
-        if (filmDto.getDirectors() == null) {
-            return;
-        }
-        directorService.deleteDirectorOfFilm(filmDto.getId());
-        for (DirectorDto directorDto : filmDto.getDirectors()) {
-            Director director = DirectorMapper.MAPPER.mapToModel(directorDto);
-            directorService.addDirectorForFilm(filmDto.getId(), director);
-        }
-    }
 
+    // удаляет все связки режиссера с фильмом и добавляет заново
     public void deleteFilmById(long filmId) {
         boolean deleted = filmStorage.deleteFilmById(filmId);
         if (!deleted) {
@@ -142,19 +119,24 @@ public class FilmService {
     }
 
     public void likeFilm(long filmId, long userId) {
-        likeFilmAction(filmId, userId, true);
+        filmStorage.likeFilm(filmId, userId);
     }
 
     public void unlikeFilm(long filmId, long userId) {
-        likeFilmAction(filmId, userId, false);
+        filmStorage.unlikeFilm(filmId, userId);
     }
 
-    private void likeFilmAction(long filmId, long userId, boolean isLike) {
-        if (isLike) {
-            filmStorage.likeFilm(filmId, userId);
-        } else {
-            filmStorage.unlikeFilm(filmId, userId);
+    private void addDirectorForFilm(Film film) {
+        if (film.getDirectors() == null) {
+            return;
         }
+        film.getDirectors().stream()
+                .map(DirectorDto::getId)
+                .forEach(directorId -> {
+                    DirectorDto directorDto = directorService.findDirectorById(directorId);
+                    Director director = DirectorMapper.MAPPER.mapToModel(directorDto);
+                    directorService.addDirectorForFilm(film.getId(), director);
+                });
     }
 
     private FilmDto setMpaName(FilmDto filmDto) {
@@ -193,5 +175,20 @@ public class FilmService {
                 .collect(Collectors.toSet());
         filmDto.setDirectors(directorsWithName);
         return filmDto;
+    }
+
+    private void convertDuration(FilmDto request) {
+        request.setDuration(request.getDuration().multipliedBy(60));
+    }
+
+    private void updateDirectorOfFilm(FilmDto filmDto) {
+        if (filmDto.getDirectors() == null) {
+            return;
+        }
+        directorService.deleteDirectorOfFilm(filmDto.getId());
+        for (DirectorDto directorDto : filmDto.getDirectors()) {
+            Director director = DirectorMapper.MAPPER.mapToModel(directorDto);
+            directorService.addDirectorForFilm(filmDto.getId(), director);
+        }
     }
 }
